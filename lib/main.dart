6d12fb3ffb211c62637bca7f11fcc8677827e8de
KASRA10 <kasrasparrow@gmail.comm>
2024-10-14 14:44:34 +0330

    Add App Icon FOr Android And IOS

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:just_audio/just_audio.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: XylophoneTwo(),
+    ),
+  );
+} // End Of Main
+
+class XylophoneTwo extends StatelessWidget {
+  const XylophoneTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
